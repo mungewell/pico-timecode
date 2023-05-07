774316b532eea3141b9b8b43d165fd11a1440586
Simon Wood <simon@mungewell.org>
2023-05-07 10:49:15 -0600

    Add function/PIO for receiving and decoding LTC

--- pico-timecode.py
+++ pico-timecode.py
@@ -8,6 +8,32 @@ import _thread
 import utime
 import rp2
 
+@rp2.asm_pio()
+
+def auto_start():
+    irq(clear, 4)                   # just Trigger Sync...
+
+    label("halt")
+    jmp("halt") [31]
+
+
+@rp2.asm_pio()
+
+def start_from_pin():
+    label("high")
+    jmp(pin, "high")                # Wait for pin to go low first...
+
+    wrap_target()
+    jmp(pin, "start")               # Check pin, jump if 1
+    wrap()
+
+    label("start")
+    irq(clear, 4)                   # Trigger Sync
+
+    label("halt")
+    jmp("halt") [31]
+
+
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW, autopull=True, out_shiftdir=rp2.PIO.SHIFT_RIGHT)
 
 def blink_led():
@@ -75,30 +101,70 @@ def buffer_out():
     wrap()
 
 
-@rp2.asm_pio()
-    
-def auto_start():
-    irq(clear, 4)                   # just Trigger Sync...
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
 
-    label("halt")
-    jmp("halt") [31]
+def decode_dmc():
+    label("initial_high")
+    wait(1, pin, 0)
+    irq(clear, 5)[10]        # trigger sync engine, and wait til 3/4s mark
 
-
-@rp2.asm_pio()
-    
-def start_from_pin():
-    label("high")
-    jmp(pin, "high")                # Wait for pin to go low first...
-
+    jmp(pin, "high_0")
+    label("high_1")
+    set(pins, 1)            # Second transition detected (a `1` data symbol)
+    jmp("initial_high")
+    label("high_0")
+    set(pins, 0)            # Line still high, no centre transition (data is `0`)
+                            # fall through... a few cycles early
     wrap_target()
-    jmp(pin, "start")               # Check pin, jump if 1
+    label("initial_low")
+    wait(0, pin, 0)
+    irq(clear, 5)[10]        # trigger sync engine, and wait til 3/4s mark
+
+    jmp(pin, "low_1")
+    label("low_0")
+    set(pins, 0)            # Line still low, no centre transition (data is `0`)
+    jmp("initial_high")
+    label("low_1")
+    set(pins, 1)            # Second transition detected (data is `1`)
+
     wrap()
 
-    label("start")
-    irq(clear, 4)                   # Trigger Sync
 
-    label("halt")
-    jmp("halt") [31]
+@rp2.asm_pio(set_init=rp2.PIO.OUT_HIGH, out_init=rp2.PIO.OUT_LOW,
+             autopull=True, autopush=True, in_shiftdir=rp2.PIO.SHIFT_RIGHT)
+
+def sync_and_read():
+    out(y, 32)              # Read the expected sync word to Y
+
+    wrap_target()
+    set(x, 0)
+
+    label("find_sync")
+    mov(isr, x)             # force X value back into ISR, clears counter
+    irq(block, 5)           # wait for input, databit ready
+    set(pins, 1)[3]         # signal 'header section' start
+
+    in_(pins, 1)            # Double clock input (ie duplicate bits)
+    in_(pins, 1)            # auto-push will NOT trigger...
+    mov(x, isr)
+
+    jmp(x_not_y, "find_sync")
+
+    set(x, 31)[10]          # Read in the next 31 bits
+    mov(isr, null)          # clear ISR
+    set(pins, 0)            # signal 'data section' start
+
+    label("next_bit")
+    in_(pins, 1)
+    jmp(x_dec, "next_bit")[14]
+
+    set(x, 31)              # Read in the next 31 bits
+
+    label("next_bit2")
+    in_(pins, 1)
+    jmp(x_dec, "next_bit2")[13]
+
+    wrap()
 
 
 #---------------------------------------------
@@ -272,6 +338,12 @@ def pico_timecode_thread(tc, rc, sm):
     
     # Loop, increasing frame count each time
     while still_running:
+
+        # Empty RX FIFO as it fills
+        if sm[5].rx_fifo() > 0:
+            sm[5].get()
+            #print("0x%x" % sm[5].get())
+
         # Wait for TX FIFO to be empty enough
         if sm[2].tx_fifo() < 5:
             # build LTC frame
@@ -292,12 +364,12 @@ def pico_timecode_thread(tc, rc, sm):
             tc.acquire()
             # Send values as 32bit
             if not start_sent:
-                sm[1].put(304)			# 1st cycle 'extra sync' correction
+                sm[1].put(304)          # 1st cycle 'extra sync' correction
             else:
-                sm[1].put(253)			# '253' is perfect length
+                sm[1].put(253)          # '253' is extact loop length
                 
             if tc.ff == 0:
-                sm[1].put(210)			# Y - duration of flash
+                sm[1].put(210)          # Y - duration of flash
             else:
                 sm[1].put(0)
             '''
@@ -313,6 +385,13 @@ def pico_timecode_thread(tc, rc, sm):
                     sm[1].put(0x0000007D)
             '''
             tc.release()
+
+            # Pre-load 'SYNC' word into RX decoder - only needed once
+            if not start_sent:
+                # needs to be bit doubled
+                # 0xBFFC -> 0xCFFFFFF0
+                sm[5].put(3489660912)
+                utime.sleep(0.005)
 
             # We can start StateMachines now that they have some data queued to send
             if not start_sent:
@@ -333,14 +412,16 @@ def pico_timecode_thread(tc, rc, sm):
             # Calculate next frame value
             tc.next_frame()
             
-        utime.sleep(0.01)
+        utime.sleep(0.001)
+
 
 def ascii_display_thread(tc, rc):
     while still_running:
         print(tc.to_ascii())
         
         utime.sleep(0.1)
-    
+
+
 #---------------------------------------------
 
 if __name__ == "__main__":
@@ -362,19 +443,29 @@ if __name__ == "__main__":
     sm.append(rp2.StateMachine(3, auto_start, freq=80 * fps * 16))
     '''
     sm.append(rp2.StateMachine(3, start_from_pin, freq=80 * fps * 16,
-                           jmp_pin=machine.Pin(13)))
+                           jmp_pin=machine.Pin(18)))
     '''
 
     # TX State Machines
     sm.append(rp2.StateMachine(0, blink_led, freq=80 * fps * 16,
                            set_base=machine.Pin(25)))       # LED on Pico board
     sm.append(rp2.StateMachine(1, buffer_out, freq=80 * fps * 16,
-                           out_base=machine.Pin(19)))       # Output of 'raw' bitstream
+                           out_base=machine.Pin(17)))       # Output of 'raw' bitstream
     sm.append(rp2.StateMachine(2, encode_dmc, freq=80 * fps * 16,
-                           jmp_pin=machine.Pin(19),
-                           in_base=machine.Pin(18),
-                           out_base=machine.Pin(18)))       # Encoded LTC Output
+                           jmp_pin=machine.Pin(17),
+                           in_base=machine.Pin(19),         # same as Output
+                           out_base=machine.Pin(19)))       # Encoded LTC Output
 
+    # RX State Machines
+    sm.append(rp2.StateMachine(4, decode_dmc, freq=80 * fps * 16,
+                           jmp_pin=machine.Pin(19),
+                           in_base=machine.Pin(19),
+                           set_base=machine.Pin(16)))       # Decoded LTC Input
+    sm.append(rp2.StateMachine(5, sync_and_read, freq=80 * fps * 16,
+                           jmp_pin=machine.Pin(16),
+                           in_base=machine.Pin(16),
+                           out_base=machine.Pin(18),
+                           set_base=machine.Pin(18)))       # 'raw' bitstream Input
 
     # Start up threads
     still_running = True

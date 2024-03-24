69c93bf230475104289ebe8b0eb0972387ff770d
Simon Wood <simon@mungewell.org>
2024-03-24 14:37:12 -0600

    Remove previous patch, now in 'main.py'

--- libs/umenu.py
+++ libs/umenu.py
@@ -352,11 +352,6 @@ class Menu:
             self._update_display(screen._items)
 
     def move(self, direction: int = 1):
-        if direction > 1 and type(self.current_screen) is not ValueItem:
-                if self.current_screen.selected + 1 == self.current_screen.count():
-                    self.current_screen.selected = 0
-                    return
-        
         self.current_screen.up() if direction < 0 else self.current_screen.down()
         self.draw()
 
